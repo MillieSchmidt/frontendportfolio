ed29dedcf6a3c89cce021796b650fac206aebe67
user.email <millenium2124@gmail.com>
2021-05-16 11:17:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+// var output = document.getElementById("output");  
+// function getlocation() {  
+//     if(navigator.geolocation){  
+//         navigator.geolocation.getCurrentPosition(displayPosition)  
+//       }  
+//     else  
+//     {  
+//          alert("Your browser does not support Geolocation API.")  
+//      }
+//       }  
+   
+//  function displayPosition(position){  
+//    var output = "<br>Your current location details<br>" + "Latitude: " + position.coords.latitude + "<br>" + "Longitude: " +    position.coords.longitude;  
+//    document.getElementById("output").innerHTML = output;
+//    var mapurl = "https://www.google.com/maps/@" + position.coords.latitude + "," + position.coords.longitude + ",14z";
+//    document.getElementById("mapurl").innerHTML =  "<a href='" + mapurl +"' target='_new'> Click here to see your location on the map.</a>";
+
+//  }  
+
+// TOP OF PAGE CODE
+
+document.onscroll = () => {
+    const progressBar = document.querySelector('#progress');
+    const distanceTop = window.pageYOffset;
+    const pageHeight = document.body.scrollHeight - window.innerHeight;
+
+    progressBar.style.width = (distanceTop / pageHeight) * 100 + '%';
+
+    const navBar = document.querySelector('nav');
+    const elem = document.getElementById('changeNav');
+    const divTop = elem.getBoundingClientRect().top;
+    const scrolled = window.pageYOffset;
+    
+    const skyblue = document.querySelector('.skyblue');
+    const blue = document.querySelector('.blue');
+    const darkblue = document.querySelector('.darkblue');
+
+    if (scrolled > divTop) {
+        navBar.style.border = 'none';
+        navBar.classList.add('bg-honey');
+        skyblue.style.animation = 'moveToLeft 1s ease-out forwards';
+        blue.style.animation = 'makeOpaque 5s ease-in forwards';
+        darkblue.style.animation = 'moveToRight 5s ease-out forwards';
+    } else {
+        navBar.style.borderBottom = '1px solid thistle';
+        navBar.classList.remove('bg-honey');
+    }
+}
+
+document.body.onload = () => {
+    const navBar = document.querySelector('nav');
+    const elem = document.getElementById('changeNav');
+    const divTop = elem.getBoundingClientRect().top;
+    const scrolled = window.pageYOffset;
+
+    const skyblue = document.querySelector('.skyblue');
+    const blue = document.querySelector('.blue');
+    const darkblue = document.querySelector('.darkblue');
+
+    if (scrolled > divTop) {
+        navBar.style.border = "none";
+        navBar.classList.add('bg-honey');
+        skyblue.style.animation = 'moveToLeft 2s ease-out forwards';
+        blue.style.animation = 'makeOpaque 5s ease-in forwards';
+        darkblue.style.animation = 'moveToRight 5s ease-out forwards';
+    } else {
+        navBar.style.borderBottom = "1px solid thistle";
+        navBar.classList.remove('bg-honey');
+    }
+}
+
+// CAROUSEL CODE
+
+const project = document.querySelectorAll('.box');
+let currentlyActive = 0;
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+// FUNCTIONS FOR CAROUSEL
+
+function backToStart() {
+    project[currentlyActive].classList.remove('active');
+    currentlyActive = 0;
+    project[currentlyActive].classList.add('active');
+}
+
+function nextSlide() {
+    project[currentlyActive].classList.remove('active');
+    currentlyActive++;
+    project[currentlyActive].classList.add('active');
+}
+
+function backToEnd() {
+    project[currentlyActive].classList.remove('active');
+    currentlyActive = project.length - 1;
+    project[currentlyActive].classList.add('active');
+}
+
+function prevSlide() {
+    project[currentlyActive].classList.remove('active');
+    currentlyActive--;
+    project[currentlyActive].classList.add('active');
+}
+
+// ARROW CLICK EVENTS FOR CAROUSEL
+
+prevBtn.addEventListener('click', function() {
+    if (currentlyActive === 0) {
+        backToEnd();
+    } else {
+        prevSlide();
+    }
+});
+
+nextBtn.addEventListener('click', function() {
+    if (currentlyActive === project.length - 1) {
+        backToStart();
+    } else {
+        nextSlide();
+    }
+});
