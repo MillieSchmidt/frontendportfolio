5562cee312923001abd00cc5cbc30d68efa867e8
user.email <millenium2124@gmail.com>
2021-05-17 17:06:07 +0300

    removed background div to fix layout

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// var output = document.getElementById("output");  
-// function getlocation() {  
-//     if(navigator.geolocation){  
-//         navigator.geolocation.getCurrentPosition(displayPosition)  
-//       }  
-//     else  
-//     {  
-//          alert("Your browser does not support Geolocation API.")  
-//      }
-//       }  
-   
-//  function displayPosition(position){  
-//    var output = "<br>Your current location details<br>" + "Latitude: " + position.coords.latitude + "<br>" + "Longitude: " +    position.coords.longitude;  
-//    document.getElementById("output").innerHTML = output;
-//    var mapurl = "https://www.google.com/maps/@" + position.coords.latitude + "," + position.coords.longitude + ",14z";
-//    document.getElementById("mapurl").innerHTML =  "<a href='" + mapurl +"' target='_new'> Click here to see your location on the map.</a>";
-
-//  }  
-
 // TOP OF PAGE CODE
 
 document.onscroll = () => {
